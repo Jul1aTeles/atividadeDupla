b7cfa50e6d9199c05346ee8b8f9a5470280d87cb
Julia Teles <2025319924@aluno.poa.ifrs.edu.br>
2026-06-02 16:53:20 -0300

    mensagem

--- questoes/0.py
+++ questoes/0.py
@@ -1,3 +1,4 @@
 print("OIIIIIIi Juliaaaaaaaaaaaaaaaaaaaaaa")
 print("OIIIIIII DAFNYYY")
 print("Amanhã tem prova de arts, isso parece o whats da programação!!")
+print("KAKAKAKAKAKKAK afe juro\n como prova se ela nem deu conteudo")
