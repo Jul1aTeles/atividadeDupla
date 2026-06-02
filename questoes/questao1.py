9b491435ed815c2cb2418e7d895d19c0d71f9504
Julia Teles <2025319924@aluno.poa.ifrs.edu.br>
2026-06-02 17:19:54 -0300

    criei o arquivo questao1

---
+++ questoes/questao1.py
@@ -0,0 +1,32 @@
+class CapsulaDoTempo():
+    def __init__(self,autor, mensagem, ano_abertura, ano_atual):
+        self.autor = autor
+        self.mensagem = mensagem
+        self.ano_abertura = ano_abertura
+        self.ano_atual = ano_atual
+    def pode_abrir(self):
+        if self.ano_abertura == self.ano_atual:
+            return f"Cápsula já pode ser aberta."
+        else:
+            return f"Cápsula ainda não pode ser aberta."
+    def calcular_espera(self):
+        if self.ano_abertura <= self.ano_atual:
+            return 0
+        else:
+            return self.ano_abertura - self.ano_atual
+    def classificar_espera(self):
+        resto = self.ano_abertura - self.ano_atual
+        if resto <= 0:
+            print("Já pode abrir")
+        elif resto >=1 and resto<=3:
+            print("Espera curta")
+        else:
+            print("Espera longa")
+    def exibir_resumo(self):
+        return f" \nautor:{self.autor}\n mensagem:{self.mensagem}\n ano de abertura:{self.ano_abertura}\n ano atual: {self.ano_atual}"
+
+teste = CapsulaDoTempo("Julia","Hello",2013,2015)
+teste.pode_abrir()
+teste.calcular_espera()
+teste.classificar_espera()
+teste.exibir_resumo()
