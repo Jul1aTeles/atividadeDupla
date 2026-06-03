b1b4112e07e4f55806764af52ca702e41fcef423
Dafny <2025324147@aluno.poa.ifrs.edu.br>
2026-06-03 14:16:12 -0300

    Fiz a 04

--- questoes/questao4.py
+++ questoes/questao4.py
@@ -3,13 +3,30 @@ class MochilaDeMissao():
         self.agente= agente
         self.equip= equip
         self.cap_max = cap_max
-    def adicionar_equipamento(self, equip):
+    def adicionar_equipamento(self):
         item= input("O que tu quer colocar na tua mochila?")
         self.equip.append(item)
     def listar_equipamentos(self):
-        for item in self.equip:
-            print(item)
-
+        for equipamento in self.equip:
+            print(equipamento)
+    def contar_equipamentos(self):
+        quantidade= len(self.equip)
+        return quantidade
+    def verificar_espaco(self):
+        if self.contar_equipamentos() >= self.cap_max:
+            return"Sua mochila está cheia!"
+        else:
+            return "Não está cheia!"
+    def exibir_relatorio(self):
+        print(f"Nome: {self.agente}")
+        print(f"Total de equipamento:{self.contar_equipamentos()} ")
+        print(f"Capacidade maxima: {self.cap_max}")
+        print(f"A situação: {self.verificar_espaco()}")
 
 lista = []
-mocg(, lista)
+mochi1= MochilaDeMissao("Ana Misteriosa", [], 3)
+mochi1.adicionar_equipamento()
+mochi1.listar_equipamentos()
+mochi1.contar_equipamentos()
+mochi1.verificar_espaco()
+mochi1.exibir_relatorio()
